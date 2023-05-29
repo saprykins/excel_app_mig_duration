50c03b3c0dfc3ed12694395831546c85b0a6146a
saprykins <60004824+saprykins@users.noreply.github.com>
2023-05-29 22:47:55 +0200

    Update app_duration_w_csv.py

--- app_duration_w_csv.py
+++ app_duration_w_csv.py
@@ -1,8 +1,12 @@
 import csv
 from datetime import datetime
 
+
 # Read the CSV data
-filename = 'your_file.csv'  # Replace with the actual filename
+filename = './results/ADO_MS_duration_extract_22-290523.csv'  # Replace with the actual filename
+# filename = './results/ADO_MS_duration_extract_15-220523.csv'  # Replace with the actual filename
+output_filename = './results/apps_durations.csv'  # Replace with the desired output filename
+
 
 # Define the start and end task prefixes
 start_task_prefix = 'TA50.10.'
@@ -17,6 +21,7 @@ with open(filename, 'r') as csvfile:
     
     # Iterate over the rows
     for row in reader:
+        app_id = row['App id']
         app_name = row['App name']
         task_name = row['Task']
         start_time = row['Start time']
@@ -25,7 +30,7 @@ with open(filename, 'r') as csvfile:
         # Check if the task name starts with the specified prefixes
         if task_name.startswith(start_task_prefix):
             if app_name not in process_durations:
-                process_durations[app_name] = {'start_time': start_time, 'end_time': None}
+                process_durations[app_name] = {'app_id': app_id, 'start_time': start_time, 'end_time': None}
             else:
                 if process_durations[app_name]['start_time'] is not None:
                     process_durations[app_name]['start_time'] = min(process_durations[app_name]['start_time'], start_time)
@@ -34,7 +39,7 @@ with open(filename, 'r') as csvfile:
                     
         elif task_name.startswith(end_task_prefix):
             if app_name not in process_durations:
-                process_durations[app_name] = {'start_time': None, 'end_time': end_time}
+                process_durations[app_name] = {'app_id': app_id, 'start_time': None, 'end_time': end_time}
             else:
                 if process_durations[app_name]['end_time'] is not None:
                     process_durations[app_name]['end_time'] = max(process_durations[app_name]['end_time'], end_time)
@@ -45,8 +50,10 @@ with open(filename, 'r') as csvfile:
 for app_name, durations in process_durations.items():
     if durations['start_time']:
         start_time = datetime.strptime(durations['start_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        mig_date = start_time.strftime('%Y-%m-%d')  # Extract the date from the start time
+        process_durations[app_name]['mig_date'] = mig_date
     else:
-        start_time = None
+        process_durations[app_name]['mig_date'] = None
     
     if durations['end_time']:
         end_time = datetime.strptime(durations['end_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -60,12 +67,12 @@ for app_name, durations in process_durations.items():
         process_durations[app_name]['duration'] = None
 
 # Write the results to a CSV file
-output_filename = 'process_durations.csv'  # Replace with the desired output filename
+# output_filename = 'process_durations.csv'  # Replace with the desired output filename
 
 with open(output_filename, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['App Name', 'Duration (min)'])
+    writer.writerow(['App ID', 'App Name', 'Mig Date', 'Duration (min)'])
     for app_name, durations in process_durations.items():
-        writer.writerow([app_name, durations['duration']])
+        writer.writerow([durations['app_id'], app_name, durations['mig_date'], durations['duration']])
 
 print("Process durations have been calculated and saved to", output_filename)
